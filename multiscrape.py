7477daaf44f428ad1b2b51457ee09a8764bc2437
Euan McGill <mcgilleuan@icloud.com>
2021-06-01 17:21:32 +0200

    tuesday additions

---
+++ multiscrape.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import re
+import requests
+
+'''
+Code based on wikisc.py, but built to handle multiple files stemming 
+from one base file and a parallel language equivalent.
+
+Not yet arranged into functions.
+'''
+
+url = 'https://simple.wikipedia.org/wiki/Category:Symptoms'
+html = urlopen(url)
+soup = BeautifulSoup(html, 'html.parser')
+
+simpends = []
+englishends = []
+for link in soup.find_all('a'):
+    redirect = link.get("href","")
+    if re.match("\/wiki/", redirect) and ":" not in redirect and "Main_Page" not in redirect:
+        simpends.append("https://simple.wikipedia.org"+redirect)
+        englishends.append("https://en.wikipedia.org"+redirect)
+
+# with loop to write directly to file?
+
+for link in simpends: # change to englishends for en wikipedia
+    url = link
+    html = urlopen(url)
+    soup = BeautifulSoup(html, 'html.parser')
+    print(link) # for URLs above each article
+    for text in soup.find_all('p', limit=4):
+        article = text.get_text(' ',strip=True)
+        print(article)
+
+# for link in englishends: # change to simpends for simp wikipedia
+#     url = link
+#     try:
+#         html = urlopen(url)
+#     except:
+#         pass
+#     soup = BeautifulSoup(html, 'html.parser')
+#     print(link) # for URLs above each article
+#     for text in soup.find_all('p', limit=3):
+#         article = text.get_text(' ',strip=True)
+#         print(article)
+
+'''
+English articles tend to be around 7x longer than Simple English
+ones. Maybe we could just grab the first paragraph of the English
+Wiki pages if the coverage tends to be the same as the whole simple
+Wiki entry for a given subject
+'''
